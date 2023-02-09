fd1a1e4bc3dbd778a7dc074ce2383396f2eecc46
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-02-10 00:19:23 +0900

    :recycle: routes 구조 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import GlobalStyle from "./components/styled/global.styled";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import TodoList from "./pages/TodoList";
 import SigninForm from "./pages/SigninForm";
 import SignupForm from "./pages/SignupForm";
 
 function App() {
-  const [isSignin, setIsSignin] = useState(false);
-
   const theme = {};
 
   return (
@@ -16,14 +13,9 @@ function App() {
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          {isSignin ? (
-            <Route element={<TodoList />} />
-          ) : (
-            <>
-              <Route path="/signin" element={<SigninForm />} />
-              <Route path="/signup" element={<SignupForm />} />
-            </>
-          )}
+          <Route path="/todo" element={<TodoList />} />
+          <Route path="/signin" element={<SigninForm />} />
+          <Route path="/signup" element={<SignupForm />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
