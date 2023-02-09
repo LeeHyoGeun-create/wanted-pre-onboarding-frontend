d641075e7ea51c6db1e4aeed902686e39b940c0a
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-02-09 23:44:56 +0900

    :sparkles: 로그인 api 추가

---
+++ src/module/api.js
@@ -0,0 +1,27 @@
+export const loginFetch = async (obj) => {
+  try {
+    const responce = await fetch(
+      `${process.env.REACT_APP_API_URL}/auth/signup`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(obj),
+      }
+    );
+    if (!responce.ok) {
+      throw new Error("서버에 이상이 있습니다 status: " + responce.status);
+    }
+    const data = await responce.json();
+    console.log(data);
+    if (data.access_token) {
+      localStorage.setItem("JWT", data.access_token);
+      return true;
+    }
+
+    return false;
+  } catch (e) {
+    throw new Error(e);
+  }
+};
