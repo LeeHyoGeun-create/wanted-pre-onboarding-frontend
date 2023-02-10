53dd0ad2f17c2f93ee18c95cdbe34bd711b3a89a
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-02-10 10:04:59 +0900

    :sparkles: JWT 토큰이 없을시 /signin으로 리다이렉트

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const TodoList = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.getItem("JWT")) {
+      navigate("/signin", { replace: true });
+    }
+  }, [navigate]);
+
   return <div>todoList</div>;
 };
 
