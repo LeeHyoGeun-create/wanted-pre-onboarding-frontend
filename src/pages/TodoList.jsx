565280aa8da336109380f8d5572c419006def42e
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-02-10 12:48:31 +0900

    :sparkles: 투두 받아오기

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,14 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { getTodosFetch } from "../module/api";
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([]);
   const navigate = useNavigate();
+
+  const getTodos = async () => {
+    const value = await getTodosFetch();
+    setTodos(value);
+  };
 
   useEffect(() => {
     if (!localStorage.getItem("JWT")) {
       navigate("/signin", { replace: true });
     }
   }, [navigate]);
+
+  useEffect(() => {
+    getTodos();
+  }, []);
 
   return <div>todoList</div>;
 };
