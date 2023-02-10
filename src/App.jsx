c244b9975654706d707fca2028ba9d62cdabeeb4
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-02-10 16:34:01 +0900

    :fire: styled components 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import GlobalStyle from "./components/styled/global.styled";
-import { ThemeProvider } from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TodoList from "./pages/TodoList";
 import SigninForm from "./pages/SigninForm";
 import SignupForm from "./pages/SignupForm";
 
 function App() {
-  const theme = {};
-
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
@@ -18,7 +15,7 @@ function App() {
           <Route path="/signup" element={<SignupForm />} />
         </Routes>
       </BrowserRouter>
-    </ThemeProvider>
+    </>
   );
 }
 
